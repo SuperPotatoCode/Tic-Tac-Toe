8b98040d9f0dea522dc441179b029695b0ca136a
Potato™ <donnyduong92@gmail.com>
2023-02-04 18:39:48 -0500

    Changed the buttons to divs
    Buttons wouldnt work with the js code

--- RRsTicTacToe/js/main.js
+++ RRsTicTacToe/js/main.js
@@ -28,6 +28,76 @@ window.addEventListener('DOMContentloaded', ()=> {
         [2][4][6]
     ];
 
+    function handleResultValidation() {
+        let roundWon = false;
+        for(let i = 0; i <=7; i++){
+            const winCondition =winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[0]];
+            const c = board[winCondition[0]];
+            if(a === '' || b === '' || c === ''){
+                continue;
+            }
+            if(a === b && b === c){
+            roundWon = true;
+            break;
+            }
+        }   
+    
+
+        if(roundWon){
+            result(currentPlayer === 'X' ? xWin : oWin);
+            isGameActive = false;
+            return
+        }
+
+        if(!board.includes(''))
+            result(t)
+
+    }
+
+    const result = (type) => {
+        switch(type){
+            case xWin:
+                result.innerHTML = `Player <span class="playerX">X</span> won`;
+                break;
+            case oWin:
+                result.innerHTML = `Player <span class="playerO">O</span> won`;
+                break;
+            case t:
+                result.innerHTML = `Tie`;
+        }
+        result.classList.remove('hide')
+    }
+
+    const isValidAction = tile => {
+        if(tile.innerText === "X" || tile.innerText === "O"){
+            return false;
+        }
+        return true; 
+    }
+
+    const updateBoard = index => {
+        board[index] = currentPlayer;  
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; 
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`); 
+    } 
+
+    const userAction = (tile, index) =>{
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer(); 
+        }
+    }
+
     tiles.forEach((tile, index) => {
         tile.addEventListener('click', () => userAction(tile,index));
     });
@@ -42,5 +112,14 @@ window.addEventListener('DOMContentloaded', ()=> {
         result.classList.add('hide');
         tiles.innerText = '';
     }
+    
+    if(currentPlayer === 'O'){
+        changePlayer();
+    }
 
+    tiles.forEach(tile => {
+        tile.innerText = '';
+        tile.classList.remove('playerX');
+        tile.classList.remove('playerO'); 
+    })
 })
