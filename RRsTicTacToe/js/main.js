dff5ff777041ed16a074e88327a286855a40353a
Potato™ <donnyduong92@gmail.com>
2023-02-04 21:29:08 -0500

    Line 1 ... l -> L
    Line 1 little 'l'  was fucking with me - COULDNT UNDERSTAND WHY MY CODE DIDNT WORK!!! ITS SUPPOSED TO BE BIG L LOL
    
    changed array win conditions
    X and O's work now.
    
    No Reset yet hmm.

--- RRsTicTacToe/js/main.js
+++ RRsTicTacToe/js/main.js
@@ -1,6 +1,6 @@
-window.addEventListener('DOMContentloaded', ()=> {
+window.addEventListener('DOMContentLoaded', () => {
     const tiles = Array.from(document.querySelectorAll('.tile'));
-    const displayPlayer = document.querySelector('.display-player');
+    const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.result');
 
@@ -8,33 +8,33 @@ window.addEventListener('DOMContentloaded', ()=> {
     let currentPlayer = 'X';
     let isGameActive = true;
 
-    const xWin = 'Player X Won';
-    const oWin = 'Player O Won';
-    const t = 'Tied Up Potatoes';
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
 
-    //Win Conditions 
+    //Board Reference
     // [0][1][2]
     // [3][4][5]
     // [6][7][8]
 
-    const winConditions = [
-        [0][1][2], 
-        [3][4][5],
-        [6][7][8],
-        [0][3][6],
-        [1][4][7],
-        [2][5][8],
-        [0][4][8],
-        [2][4][6]
+    const winningConditions = [
+        [0,1,2], 
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
     ];
 
     function handleResultValidation() {
         let roundWon = false;
         for(let i = 0; i <=7; i++){
-            const winCondition =winningConditions[i];
+            const winCondition = winningConditions[i];
             const a = board[winCondition[0]];
-            const b = board[winCondition[0]];
-            const c = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
             if(a === '' || b === '' || c === ''){
                 continue;
             }
@@ -46,9 +46,9 @@ window.addEventListener('DOMContentloaded', ()=> {
     
 
         if(roundWon){
-            result(currentPlayer === 'X' ? xWin : oWin);
+            result(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
             isGameActive = false;
-            return
+            return;
         }
 
         if(!board.includes(''))
@@ -58,26 +58,26 @@ window.addEventListener('DOMContentloaded', ()=> {
 
     const result = (type) => {
         switch(type){
-            case xWin:
-                result.innerHTML = `Player <span class="playerX">X</span> won`;
+            case PLAYERX_WON:
+                announcer.innerHTML = `Player <span class="playerX">X</span> won`;
                 break;
-            case oWin:
-                result.innerHTML = `Player <span class="playerO">O</span> won`;
+            case PLAYERO_WON:
+                announcer.innerHTML = `Player <span class="playerO">O</span> won`;
                 break;
-            case t:
-                result.innerHTML = `Tie`;
+            case TIE :
+                announcer.innerText = `Tie`;
         }
-        result.classList.remove('hide')
+        announcer.classList.remove('hide')
     }
 
-    const isValidAction = tile => {
+    const isValidAction = (tile) => {
         if(tile.innerText === "X" || tile.innerText === "O"){
             return false;
         }
         return true; 
     }
 
-    const updateBoard = index => {
+    const updateBoard = (index) => {
         board[index] = currentPlayer;  
     }
 
@@ -104,13 +104,11 @@ window.addEventListener('DOMContentloaded', ()=> {
 
     resetButton.addEventListener('click', resetBoard); 
 
-
     const resetBoard = () => {
         board = ['','','','','','','','',''];
-        currentPlayer = 'X';
         isGameActive = true;
-        result.classList.add('hide');
-        tiles.innerText = '';
+        announcer.classList.add('hide');
+      
     }
     
     if(currentPlayer === 'O'){
